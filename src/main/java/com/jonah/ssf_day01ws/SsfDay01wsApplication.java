14851aa0358a7fdaf1fa1d016d20083c64dd68d2
jonahng <nyhj1998@gmail.com>
2024-11-17 14:16:47 +0800

    spring template with index.html

---
+++ src/main/java/com/jonah/ssf_day01ws/SsfDay01wsApplication.java
@@ -0,0 +1,13 @@
+package com.jonah.ssf_day01ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsfDay01wsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsfDay01wsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonah/ssf_day01ws/SsfDay01wsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonah.ssf_day01ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsfDay01wsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
