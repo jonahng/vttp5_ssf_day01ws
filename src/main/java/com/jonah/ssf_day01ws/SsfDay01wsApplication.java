81fa29d085eef0cbf061ac921cfc63ef49b0b1bb
jonahng <nyhj1998@gmail.com>
2024-11-17 14:28:45 +0800

    running server

--- src/main/java/com/jonah/ssf_day01ws/SsfDay01wsApplication.java
+++ src/main/java/com/jonah/ssf_day01ws/SsfDay01wsApplication.java
@@ -1,5 +1,9 @@
 package com.jonah.ssf_day01ws;
 
+import java.util.Collections;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.DefaultApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +11,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SsfDay01wsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SsfDay01wsApplication.class, args);
+		SpringApplication app = new SpringApplication(SsfDay01wsApplication.class);
+		String port = "3000";
+		ApplicationArguments argsOptions = new DefaultApplicationArguments(args);
+
+		if(argsOptions.containsOption("port")){
+			port = argsOptions.getOptionValues("port").get(0);
+		}
+
+		app.setDefaultProperties(Collections.singletonMap("server.port", port));
+		app.run(args);
 	}
 
 }
